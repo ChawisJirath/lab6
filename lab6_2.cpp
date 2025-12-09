d3ff4745c30cf53a5c4ffcf31f2f5054e60269c2
Wia <chawis_jirath@cmu.ac.th>
2025-12-09 16:08:35 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
+double deg2rad(double deg){
+    return (deg*M_PI)/180.0;
+}
+double rad2deg(double rad){
+    return (rad*180.0)/M_PI;
+}
+double findXComponent(double l1,double l2,double a1,double a2){
+    return (l1*cos(a1))+(l2*cos(a2));
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+    return (l1*sin(a1))+(l2*sin(a2));
+}
+double pythagoras(double xcomp,double ycomp){
+    return sqrt((xcomp*xcomp)+(ycomp*ycomp));
+}
+void showResult(double length,double direction){
+    cout << "Length of the resultant vector = " << length << endl;
+    cout << "Direction of the resultant vector (deg) = " << direction << endl;
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -19,6 +38,8 @@ int main(){
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
-
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
     showResult(result_vec_length,result_vec_direction);
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    return 0;
 }
